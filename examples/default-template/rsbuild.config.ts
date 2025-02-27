dc36c30a07d1c33d88c9a2a295ebfd6c9a696e74
ScriptedAlchemy <zackaryjackson@bytedance.com>
2025-02-27 12:53:06 -0800

    chore: add e2e

--- examples/default-template/rsbuild.config.ts
+++ examples/default-template/rsbuild.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
 import { pluginReactRouter } from '@rsbuild/plugin-react-router';
 import 'react-router';
-
+import path from 'path';
 declare module 'react-router' {
   interface AppLoadContext {
     VALUE_FROM_EXPRESS: string;
@@ -11,6 +11,11 @@ declare module 'react-router' {
 
 export default defineConfig(() => {
   return {
-    plugins: [pluginReactRouter(), pluginReact()],
+    dev: {
+      watchFiles: {
+        paths: ['app/**/*'],
+      },
+    },
+    plugins: [pluginReactRouter({serverOutput: 'commonjs'}), pluginReact()],
   };
 });
