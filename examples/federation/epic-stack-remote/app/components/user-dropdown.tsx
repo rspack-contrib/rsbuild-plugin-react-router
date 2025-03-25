0a62313996386be5e8240e13e950865ebcc78078
ScriptedAlchemy <zackaryjackson@bytedance.com>
2025-03-25 00:17:58 -0700

    chore: add border for remote modules

--- examples/federation/epic-stack-remote/app/components/user-dropdown.tsx
+++ examples/federation/epic-stack-remote/app/components/user-dropdown.tsx
@@ -17,54 +17,54 @@ export function UserDropdown() {
 	const formRef = useRef<HTMLFormElement>(null)
 	return (
 		<div className="border-2 border-red-500">
-			<DropdownMenu>
-				<DropdownMenuTrigger asChild>
-					<Button asChild variant="secondary">
-						<Link
-							to={`/users/${user.username}`}
-							// this is for progressive enhancement
-							onClick={(e) => e.preventDefault()}
-							className="flex items-center gap-2"
-						>
-							<img
-								className="h-8 w-8 rounded-full object-cover"
-								alt={user.name ?? user.username}
-								src={getUserImgSrc(user.image?.id)}
-							/>
-							<span className="text-body-sm font-bold">
-								{user.name ?? user.username}
-							</span>
+		<DropdownMenu>
+			<DropdownMenuTrigger asChild>
+				<Button asChild variant="secondary">
+					<Link
+						to={`/users/${user.username}`}
+						// this is for progressive enhancement
+						onClick={(e) => e.preventDefault()}
+						className="flex items-center gap-2"
+					>
+						<img
+							className="h-8 w-8 rounded-full object-cover"
+							alt={user.name ?? user.username}
+							src={getUserImgSrc(user.image?.id)}
+						/>
+						<span className="text-body-sm font-bold">
+							{user.name ?? user.username}
+						</span>
+					</Link>
+				</Button>
+			</DropdownMenuTrigger>
+			<DropdownMenuPortal>
+				<DropdownMenuContent sideOffset={8} align="end">
+					<DropdownMenuItem asChild>
+						<Link prefetch="intent" to={`/users/${user.username}`}>
+							<Icon className="text-body-md" name="avatar">
+								Profile
+							</Icon>
 						</Link>
-					</Button>
-				</DropdownMenuTrigger>
-				<DropdownMenuPortal>
-					<DropdownMenuContent sideOffset={8} align="end">
+					</DropdownMenuItem>
+					<DropdownMenuItem asChild>
+						<Link prefetch="intent" to={`/users/${user.username}/notes`}>
+							<Icon className="text-body-md" name="pencil-2">
+								Notes
+							</Icon>
+						</Link>
+					</DropdownMenuItem>
+					<Form action="/logout" method="POST" ref={formRef}>
 						<DropdownMenuItem asChild>
-							<Link prefetch="intent" to={`/users/${user.username}`}>
-								<Icon className="text-body-md" name="avatar">
-									Profile
+							<button type="submit" className="w-full">
+								<Icon className="text-body-md" name="exit">
+									Logout
 								</Icon>
-							</Link>
+							</button>
 						</DropdownMenuItem>
-						<DropdownMenuItem asChild>
-							<Link prefetch="intent" to={`/users/${user.username}/notes`}>
-								<Icon className="text-body-md" name="pencil-2">
-									Notes
-								</Icon>
-							</Link>
-						</DropdownMenuItem>
-						<Form action="/logout" method="POST" ref={formRef}>
-							<DropdownMenuItem asChild>
-								<button type="submit" className="w-full">
-									<Icon className="text-body-md" name="exit">
-										Logout
-									</Icon>
-								</button>
-							</DropdownMenuItem>
-						</Form>
-					</DropdownMenuContent>
-				</DropdownMenuPortal>
-			</DropdownMenu>
+					</Form>
+				</DropdownMenuContent>
+			</DropdownMenuPortal>
+		</DropdownMenu>
 		</div>
 	)
 }
