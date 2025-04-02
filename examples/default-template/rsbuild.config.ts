4d1f8f0973bd6414126691bcd269061080a3664d
ScriptedAlchemy <zackaryjackson@bytedance.com>
2025-04-02 15:50:09 -0700

    chore: fix startup script

--- examples/default-template/rsbuild.config.ts
+++ examples/default-template/rsbuild.config.ts
@@ -11,6 +11,6 @@ declare module 'react-router' {
 
 export default defineConfig(() => {
   return {
-    plugins: [pluginReactRouter({serverOutput: "module"}), pluginReact()],
+    plugins: [pluginReactRouter({serverOutput: "commonjs"}), pluginReact()],
   };
 });
